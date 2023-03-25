7f11db266112fae81a40d4af60f9af920980f13a
CrioUser <crio-user@criodo.com>
2023-03-25 15:42:43 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,27 +10,34 @@ import "./Register.css";
 
 const Register = () => {
   const { enqueueSnackbar } = useSnackbar();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [loading, setLoading] = useState(false);
 
   const register = async (formData) => {
     setLoading(true);
+    if (!validateInput(formData)) return;
     try {
-      const response = await axios.post(
-        `${config.endpoint}/auth/register`,
-        formData
-      );
-      if (response.data.success) {
-        enqueueSnackbar("Registration successful", { variant: "success" });
-      } else {
-        enqueueSnackbar(response.data.message, { variant: "error" });
-      }
+      await axios.post(`${config.endpoint}/auth/register`, {
+        username: formData.username,
+        password: formData.password,
+      });
+      enqueueSnackbar("Registration successful", { variant: "success" });
+      setLoading(false);
     } catch (error) {
-      if (error.response.status) {
-        enqueueSnackbar("Username is already taken", { variant: "error" });
+      if (error.response.status === 400 && error.response) {
+        return enqueueSnackbar(error.response.data.message, {
+          variant: "error",
+        });
       } else {
-        enqueueSnackbar(error.response.data.message, { variant: "error" });
+        enqueueSnackbar(
+          "Something went wrong. Check the backend is running, reachable and return valid JSON",
+          { variant: "error" }
+        );
       }
-    } finally {
       setLoading(false);
     }
   };
@@ -39,36 +46,28 @@ const Register = () => {
     if (!data.username) {
       enqueueSnackbar("Username is a required field", { variant: "warning" });
       return false;
-    } else if (data.username.length < 6) {
+    }
+    if (data.username.length < 6) {
       enqueueSnackbar("Username must be at least 6 characters", {
         variant: "warning",
       });
       return false;
-    } else if (!data.password) {
+    }
+    if (!data.password) {
       enqueueSnackbar("Password is a required field", { variant: "warning" });
       return false;
-    } else if (data.password.length < 6) {
+    }
+    if (data.password.length < 6) {
       enqueueSnackbar("Password must be at least 6 characters", {
         variant: "warning",
       });
       return false;
-    } else if (data.password !== data.confirmPassword) {
+    }
+    if (data.password !== data.confirmPassword) {
       enqueueSnackbar("Passwords do not match", { variant: "warning" });
       return false;
     }
     return true;
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const formData = {
-      username: event.target.username.value,
-      password: event.target.password.value,
-      confirmPassword: event.target.confirmPassword.value,
-    };
-    if (validateInput(formData)) {
-      await register(formData);
-    }
   };
 
   return (
@@ -79,54 +78,66 @@ const Register = () => {
       minHeight="100vh"
     >
       <Header hasHiddenAuthButtons />
-      <form onSubmit={handleFormSubmit}>
-        <Box className="content">
-          <Stack spacing={2} className="form">
-            <h2 className="title">Register</h2>
-            <TextField
-              id="username"
-              label="Username"
-              variant="outlined"
-              title="Username"
-              name="username"
-              placeholder="Enter Username"
-              fullWidth
-            />
-            <TextField
-              id="password"
-              variant="outlined"
-              label="Password"
-              name="password"
-              type="password"
-              helperText="Password must be atleast 6 characters length"
-              fullWidth
-              placeholder="Enter a password with minimum 6 characters"
-            />
-            <TextField
-              id="confirmPassword"
-              variant="outlined"
-              label="Confirm Password"
-              name="confirmPassword"
-              type="password"
-              fullWidth
-            />
-            <Button
-              className="button"
-              variant="contained"
-              type="submit"
-              disabled={loading}
-            >
-              {loading ? <CircularProgress size={24} /> : "Register Now"}
-            </Button>
-            <p className="secondary-action">
-              Already have an account?{" "}
-              <a className="link" href="#">
-                Login here
-              </a>
-            </p>
-          </Stack>
-        </Box>
-      </form>
+      <Box className="content">
+        <Stack spacing={2} className="form">
+          <h2 className="title">Register</h2>
+          <TextField
+            id="username"
+            label="Username"
+            variant="outlined"
+            title="Username"
+            name="username"
+            value={formData.username}
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+            placeholder="Enter Username"
+            fullWidth
+          />
+          <TextField
+            id="password"
+            variant="outlined"
+            label="Password"
+            name="password"
+            type="password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            helperText="Password must be atleast 6 characters length"
+            fullWidth
+            placeholder="Enter a password with minimum 6 characters"
+          />
+          <TextField
+            id="confirmPassword"
+            variant="outlined"
+            label="Confirm Password"
+            name="confirmPassword"
+            type="password"
+            onChange={(e) =>
+              setFormData({ ...formData, confirmPassword: e.target.value })
+            }
+            value={formData.confirmPassword}
+            fullWidth
+          />
+          <Button
+            className="button"
+            variant="contained"
+            onClick={async () => {
+              await register(formData);
+            }}
+            disabled={loading}
+          >
+            {loading ? <CircularProgress size={24} /> : "Register Now"}
+          </Button>
+          <p className="secondary-action">
+            Already have an account?{" "}
+            <a className="link" href="#">
+              Login here
+            </a>
+          </p>
+        </Stack>
+      </Box>
       <Footer />
     </Box>
   );
